ed3fe11f6bb21cfa595dcd3657c2e67107d20655
Sockalingamvenuka <154747063+Sockalingamvenuka@users.noreply.github.com>
2024-12-03 17:34:21 +0000

    modify this

--- ica1/src/components/Products.js
+++ ica1/src/components/Products.js
@@ -25,7 +25,9 @@ export default function Products(){
                 <h4 className="card-title">Buy flowers</h4>
                 <div className="grid-container">
                     {
-                        //product
+                        flowers.map(flower => (
+                            <Product key={flower.id} flower={flower}  addToCart={addToCart}/>
+                        ))
                     }
                 </div>
 
